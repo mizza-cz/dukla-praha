874b19b6df5382770512e04e4c1d669480793ca3
mizza-cz <mizza.cz666@gmail.com>
2023-04-26 18:12:23 +0200

    add fix

--- src/components/hp-statistics/hp-statistics.js
+++ src/components/hp-statistics/hp-statistics.js
@@ -1,6 +1,6 @@
 $(".top-score .tab").on("click", function (event) {
    var id = $(this).attr("data-id");
-   $(".top-score").find(".tab-content").removeClass("tab-active").hide();
+   $(".top-score").find(".tab-wrap").removeClass("tab-active").hide();
    $(".top-score .top-score__tabs").find(".tab").removeClass("active");
    $(this).addClass("active");
    $("#" + id)
