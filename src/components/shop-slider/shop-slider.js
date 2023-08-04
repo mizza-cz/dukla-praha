773f2c6cbe4a75fd388eb0e2660445cd026e2882
mizza-cz <mizza.cz666@gmail.com>
2023-08-04 22:23:14 +0200

    add fix slider shop

--- src/components/shop-slider/shop-slider.js
+++ src/components/shop-slider/shop-slider.js
@@ -1,23 +1,11 @@
 $(".shop-slider__inner").slick({
-  slidesToShow: 6,
+  slidesToShow: 4,
   slidesToScroll: 1,
   prevArrow:
     '<button class="shop-slider__btn shop-slider__btnprev"><img src="images/ico/slider.svg" loading="lazy" alt="" /></button> ',
   nextArrow:
     ' <button class="shop-slider__btn  shop-slider__btnnext"><img src="images/ico/slider.svg" loading="lazy" alt="" /></button>',
   responsive: [
-    {
-      breakpoint: 1380,
-      settings: {
-        slidesToShow: 5,
-      },
-    },
-    {
-      breakpoint: 1201,
-      settings: {
-        slidesToShow: 4,
-      },
-    },
     {
       breakpoint: 1001,
       settings: {
