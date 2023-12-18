88543b224fa1703bde4e20c02ba75328dd264596
mizza-cz <mizza.cz666@gmail.com>
2023-12-18 12:30:32 +0100

    add fixes

--- src/components/shop-slider/shop-slider.js
+++ src/components/shop-slider/shop-slider.js
@@ -1,29 +1,32 @@
-$(".shop-slider__inner").slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  prevArrow:
-    '<button class="shop-slider__btn shop-slider__btnprev"><img src="images/ico/slider.svg" loading="lazy" alt="" /></button> ',
-  nextArrow:
-    ' <button class="shop-slider__btn  shop-slider__btnnext"><img src="images/ico/slider.svg" loading="lazy" alt="" /></button>',
-  responsive: [
-    {
-      breakpoint: 1001,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-
-    {
-      breakpoint: 720,
-      settings: {
-        slidesToShow: 2,
-      },
-    },
-    {
-      breakpoint: 541,
-      settings: {
-        slidesToShow: 1,
-      },
-    },
-  ],
+$(document).ready(function () {
+  if ($(".shop-slider__inner .shop-slider__col").length > 4) {
+    $(".shop-slider__inner").slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow:
+        '<button class="shop-slider__btn shop-slider__btnprev"><img src="images/ico/slider.svg" loading="lazy" alt="" /></button>',
+      nextArrow:
+        '<button class="shop-slider__btn shop-slider__btnnext"><img src="images/ico/slider.svg" loading="lazy" alt="" /></button>',
+      responsive: [
+        {
+          breakpoint: 1001,
+          settings: {
+            slidesToShow: 3,
+          },
+        },
+        {
+          breakpoint: 720,
+          settings: {
+            slidesToShow: 2,
+          },
+        },
+        {
+          breakpoint: 541,
+          settings: {
+            slidesToShow: 1,
+          },
+        },
+      ],
+    });
+  }
 });
