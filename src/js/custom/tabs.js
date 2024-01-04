f1269ee239210a5349951bbff3a3081cfdd37143
mizza-cz <mizza.cz666@gmail.com>
2024-01-04 23:42:44 +0100

    fix tabs

--- src/js/custom/tabs.js
+++ src/js/custom/tabs.js
@@ -2,7 +2,7 @@ $(function () {
   var hash = window.location.hash;
   hash && $('ul.nav a[href="' + hash + '"]').tab("show");
 
-  $(".nav-tabs a").click(function (e) {
+  $(".nav-tabs a[data-toggle='tab']").click(function (e) {
     e.preventDefault();
     e.stopImmediatePropagation();
     $(this).tab("show");
