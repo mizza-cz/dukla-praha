b584f6220f8199bfab391c47e2563cc0a50f4c80
mizza-cz <mizza.cz666@gmail.com>
2024-01-11 12:14:22 +0100

    add fixes

--- src/components/shop-slider/shop-slider.js
+++ src/components/shop-slider/shop-slider.js
@@ -1,32 +1,54 @@
 $(document).ready(function () {
-  if ($(".shop-slider__inner .shop-slider__col").length > 4) {
-    $(".shop-slider__inner").slick({
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      prevArrow:
-        '<button class="shop-slider__btn shop-slider__btnprev"><img src="images/ico/slider.svg" loading="lazy" alt="" /></button>',
-      nextArrow:
-        '<button class="shop-slider__btn shop-slider__btnnext"><img src="images/ico/slider.svg" loading="lazy" alt="" /></button>',
-      responsive: [
-        {
-          breakpoint: 1001,
-          settings: {
-            slidesToShow: 3,
-          },
+  var $slider = $(".shop-slider__inner");
+  var initialized = false;
+
+  var sliderSettings = {
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    prevArrow:
+      '<button class="shop-slider__btn shop-slider__btnprev"><img src="images/ico/slider.svg" loading="lazy" alt="" /></button>',
+    nextArrow:
+      '<button class="shop-slider__btn shop-slider__btnnext"><img src="images/ico/slider.svg" loading="lazy" alt="" /></button>',
+    responsive: [
+      {
+        breakpoint: 1001,
+        settings: {
+          slidesToShow: 3,
         },
-        {
-          breakpoint: 720,
-          settings: {
-            slidesToShow: 2,
-          },
+      },
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 2,
         },
-        {
-          breakpoint: 541,
-          settings: {
-            slidesToShow: 1,
-          },
+      },
+      {
+        breakpoint: 541,
+        settings: {
+          slidesToShow: 1,
         },
-      ],
-    });
+      },
+    ],
+  };
+
+  function checkAndInitializeSlider() {
+    var elementCount = $slider.children(".shop-slider__col").length;
+
+    if (
+      !initialized &&
+      (elementCount > 4 || (elementCount <= 4 && $(window).width() < 1001))
+    ) {
+      $slider.slick(sliderSettings);
+      initialized = true;
+    } else if (initialized && elementCount <= 4 && $(window).width() >= 1001) {
+      $slider.slick("unslick");
+      initialized = false;
+    }
   }
+
+  checkAndInitializeSlider();
+
+  $(window).on("resize", function () {
+    checkAndInitializeSlider();
+  });
 });
