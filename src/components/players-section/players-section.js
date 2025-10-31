6c0820de41e5693f229a7ad83e2c255b93ec7bfd
mizza-cz <mizza.cz666@gmail.com>
2025-10-31 07:42:04 +0100

    change juniors

--- src/components/players-section/players-section.js
+++ src/components/players-section/players-section.js
@@ -1,15 +1,18 @@
 $(document).ready(function () {
-  $('.header-section__nav a[href^="#"]').bind("click", function (e) {
-    var anchor = $(this);
-    $("html, body")
-      .stop()
-      .animate(
-        {
-          scrollTop: $(anchor.attr("href")).offset().top - -10,
-        },
-        100
-      );
-    e.preventDefault();
-  });
+  $('.header-section__nav a[href^="#"], .js-scroll a[href^="#"]').bind(
+    "click",
+    function (e) {
+      var anchor = $(this);
+      $("html, body")
+        .stop()
+        .animate(
+          {
+            scrollTop: $(anchor.attr("href")).offset().top - -10,
+          },
+          100
+        );
+      e.preventDefault();
+    }
+  );
   return false;
 });
